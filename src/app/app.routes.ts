2b286a9bfc0ecbd9a4de957969a582f1255add6b
Sharmila L <sharmi2998@gmail.com>
2026-04-18 10:35:46 +0530

    fixed rebase issue

--- src/app/app.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-import { TaskBoardComponent } from './components/todo-task-board/todo-task-board.component';
-
-export const routes: Routes = [
-  { path: '', component: TaskBoardComponent }
-];
